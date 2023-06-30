9bd83c050e56c1456f2d8d86df8ef6ff97fcbe3c
FAL-coffee <fal.engineer.2001@gmail.com>
2023-07-01 03:40:25 +0900

    :sparkles: swaggerの機構を実装

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -18,6 +19,15 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('都々逸ライフ Web API')
+    .setDescription('都々逸ライフ Web API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // start app
   const port = configService.get<number>('port');
