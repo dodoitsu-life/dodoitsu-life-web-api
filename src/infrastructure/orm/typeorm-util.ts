08094486a97d629630f682bd3995cc856f62196c
FAL-coffee <fal.engineer.2001@gmail.com>
2023-08-28 21:36:27 +0900

    :adhesive_bandage: .envの内容変更にかかわる修正

--- src/infrastructure/orm/typeorm-util.ts
+++ src/infrastructure/orm/typeorm-util.ts
@@ -2,17 +2,25 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Dodoitsu } from '../../domain/dodoitsu/dodoitsu.entity';
 import { User } from 'src/domain/user/user.entity';
-
 export const createTypeOrmOptions = async (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => {
+  const ssl = configService.get('isLocal')
+    ? false
+    : {
+        rejectUnauthorized: false,
+      };
+
   return {
     name: 'default',
     type: 'postgres',
-    url: configService.get('database.url'),
-    ssl: {
-      rejectUnauthorized: !configService.get('database.ssl'),
-    },
+    host: configService.get('database.host'),
+    port: configService.get('database.port'),
+    username: configService.get('database.username'),
+    password: configService.get('database.password'),
+    database: configService.get('database.database'),
+    logging: configService.get('database.logging'),
+    ssl,
     synchronize: configService.get('database.synchronize'),
     entities: [Dodoitsu, User],
     migrations: ['dist/migration/*.js'],
