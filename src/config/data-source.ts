8d9fa8a81e70927487b7cf5cdc77474f1bcdd756
ふぁる <73627117+FAL-coffee@users.noreply.github.com>
2023-08-28 23:54:20 +0900

    Feature/add migration integration (#7)
    * :adhesive_bandage: .envのsampleをenv/に移行　docker imageのbuild時に.envを含めず、.env.dockerから生成するよう変更
    
    * :adhesive_bandage: .envの内容変更にかかわる修正
    
    * :fire: 不要なimport
    
    * :zap: Migrationコマンド実行用のnpm scriptの追加・データソースファイルの作成
    
    * :memo: READMEを整理
    
    * :construction_worker: Procfileにmigration:runの実行を追加
    
    * :sparkles: Migrationファイルを作成 init
    
    * :adhesive_bandage: 微修正
    
    * :adhesive_bandage: Ssl

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,6 +3,12 @@ import { DataSource } from 'typeorm';
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
+  ssl:
+    process.env.DODOITSULIFE_ENV === 'local'
+      ? false
+      : {
+          rejectUnauthorized: false,
+        },
   port: parseInt(process.env.DB_PORT, 10) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
