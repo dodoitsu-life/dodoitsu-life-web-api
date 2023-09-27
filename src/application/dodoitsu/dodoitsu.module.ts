0ee9b29135240ad2489503fd63a09c6073c20d3c
ふぁる <73627117+FAL-coffee@users.noreply.github.com>
2023-09-28 02:38:51 +0900

    Feature/add profile api (#14)
    * :sparkles: userのprofile画面にかかわるAPIの実装
    
    * :fire: 不要なコードを削除

--- src/application/dodoitsu/dodoitsu.module.ts
+++ src/application/dodoitsu/dodoitsu.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
 
 import { Dodoitsu } from '@domain/dodoitsu/dodoitsu.entity';
 import { DodoitsuLike } from '@domain/dodoitsu/dodoitsu-like.entity';
 import { SYMBOL as DODOITSU_SYMBOL } from '@domain/dodoitsu/dodoitsu.repository.interface';
 
 import { User } from '@domain/user/user.entity';
-import { UserService } from '@domain/user/user.service';
-import { SYMBOL as USER_SYMBOL } from '@domain/user/user.repository.interface';
 
 import { DodoitsuController } from '@application/dodoitsu/dodoitsu.controller';
 import { DodoitsuApplicationService } from '@application/dodoitsu/dodoitsu.service';
@@ -19,13 +15,11 @@ import { DodoitsuService } from '@domain/dodoitsu/dodoitsu.service';
 import { UserModule } from '@application/user/user.module';
 
 import { DodoitsuRepository } from '@infrastructure/orm/dodoitsu/dodoitsu.repository';
-import { UserRepository } from '@infrastructure/orm/user/user.repository';
 import { OptionalJwtStrategy } from '@infrastructure/auth/optional-jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Dodoitsu, User, DodoitsuLike]),
-    PassportModule.register({ session: true }),
     JwtModule,
     ConfigModule,
     UserModule,
@@ -38,14 +32,8 @@ import { OptionalJwtStrategy } from '@infrastructure/auth/optional-jwt.strategy'
       provide: DODOITSU_SYMBOL,
       useClass: DodoitsuRepository,
     },
-    UserService,
-    {
-      provide: USER_SYMBOL,
-      useClass: UserRepository,
-    },
-    JwtService,
     OptionalJwtStrategy,
   ],
-  exports: [PassportModule],
+  exports: [],
 })
 export class DodoitsuModule {}
